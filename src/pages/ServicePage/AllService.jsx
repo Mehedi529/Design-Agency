54a11c6eda2667965a33a688d3e5e90d7b2a8f12
Mehedi529 <mehedihasan49265@gmail.com>
2023-11-19 22:30:14 +0600

    project pages updated

--- src/pages/ServicePage/AllService.jsx
+++ src/pages/ServicePage/AllService.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 
 const AllService = () => {
     return (
-        <div>
-            <h1>This is all service page</h1>
-        </div>
+            <div className=" max-w-[704px] text-center lg:text-left   rounded-[20px] shadow-md">
+                <h3 className="text-black p-5 pb-0  text-[26px] font-semibold  uppercase">Our All Services</h3>
+                <p className=" p-5 text-neutral-400 text-base font-normal  capitalize">We provide best web design services</p>
+                <div className="img-container ">
+                    <div className="flex">
+                    <img src="" className="w-9/12  " alt="" />
+                    <img src="" className="w-3/12   " alt="" />
+                    </div>
+
+                    <div className="flex">
+                    <img src="" className="w-6/12   " alt="" />
+                    <img src="" className="w-6/12 " alt="" />
+                    </div>
+                </div>
+                </div>
     );
 };
 
