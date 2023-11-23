0c392f3bab03eaa7d1bc77c294cf3a7666d05b45
Mehedi529 <mehedihasan49265@gmail.com>
2023-11-23 23:11:36 +0600

    project page updated

--- src/pages/ProjectPage/AllProject.jsx
+++ src/pages/ProjectPage/AllProject.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AllProject = () => {
     return (
         <div>
-            <h1>This is all project page</h1>
+            <h1>This is  project page</h1>
         </div>
     );
 };
